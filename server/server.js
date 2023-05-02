7dd794873b9d4a759fac27c40e45a5d24aac9a4e
Cody Fackrell <codyfackrell56@gmail.com>
2023-05-01 23:23:15 -0600

    update connection string

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`../${__dirname}/public`))
 
 
 // Endpoints
