4eade0fdd052b7a1224ec7f9c18e3d02fefded5c
Cody Fackrell <codyfackrell56@gmail.com>
2023-05-01 22:31:39 -0600

    update connection string

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,7 @@
 const {CONNECTION_STRING} = process.env;
 
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(CONNECTION_STRING, {
+const sequelize = new Sequelize(`${CONNECTION_STRING}`, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
