2add52a5bcb2da2202b4fe99e418fc1b5d9cef63
Cody Fackrell <codyfackrell56@gmail.com>
2023-09-27 21:20:08 -0600

    onClick for TrackCard, seed db

--- src/components/HomePage/TrackCard.js
+++ src/components/HomePage/TrackCard.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const TrackCard = ({track, trackImg, trackName, trackLocation}) => {
+
+  const handleCardClick = (clickedTrack) => {
+    console.log(clickedTrack)
+  }
+
   return (
-    <div className="track-card" id={track}>
+    <div className="track-card" onClick={() => handleCardClick(track)}>
       <img src={trackImg} alt={track} className="track-img" />
       <p className="track-name">{trackName}</p>
       <p className="track-location">{trackLocation}</p>
